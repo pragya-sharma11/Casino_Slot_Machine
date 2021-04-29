066124f16aea07817b3cced254e298d33601627c
github <pragya.sharma2_cs19@gla.ac.in>
2021-04-29 23:56:27 +0530

    some fixes

--- Project/script.js
+++ Project/script.js
@@ -2,10 +2,10 @@ let value1 = document.getElementsById('value1');
 let value2 = document.getElementsById('value2');
 let value3 = document.getElementsById('value3');
 
-let arrayValues = [String.fromCodePoint(0x1F60E),String.fromCodePoint(0x1F60B),String.fromCodePoint(0x1F60D),String.fromCodePoint(0x1F608),String.fromCodePoint(0x1F602),String.fromCodePoint(0x1F604)]
+let values = [ String.fromCodePoint(0x1F60E),String.fromCodePoint(0x1F60B),String.fromCodePoint(0x1F60D),String.fromCodePoint(0x1F608),String.fromCodePoint(0x1F602),String.fromCodePoint(0x1F604)];
 
 function getRadomValues(){
-    return arrayValues[Math.floor(Math.random()*6)];
+    return values[Math.floor(Math.random()*6)];
 }
 
 setInterval(()=>{
