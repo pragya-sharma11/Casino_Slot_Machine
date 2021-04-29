85391fb09f18b33576448fd66d8c5ffb6b06e8ca
github <pragya.sharma2_cs19@gla.ac.in>
2021-04-30 00:30:43 +0530

    some fixes regarding getElementById() method

--- Project/script.js
+++ Project/script.js
@@ -1,15 +1,12 @@
-let value1 = document.getElementsById('value1');
-let value2 = document.getElementsById('value2');
-let value3 = document.getElementsById('value3');
 
-var values = [ String.fromCodePoint(0x1F60E),String.fromCodePoint(0x1F60B),String.fromCodePoint(0x1F60D),String.fromCodePoint(0x1F608),String.fromCodePoint(0x1F602),String.fromCodePoint(0x1F604)];
+let values=[ String.fromCodePoint(0x1F60E), String.fromCodePoint(0x1F60B), String.fromCodePoint(0x1F60D), String.fromCodePoint(0x1F608), String.fromCodePoint(0x1F602), String.fromCodePoint(0x1F604)];
 
 function getRadomValues(){
     return values[Math.floor(Math.random()*6)];
 }
 
 setInterval(()=>{
-    value1.innerHTML=getRadomValues();
-    value2.innerHTML=getRadomValues();
-    value3.innerHTML=getRadomValues();
-}, 2000)
+    document.getElementById( 'value1' ).innerHTML=getRadomValues();
+    document.getElementById( 'value2' ).innerHTML=getRadomValues();
+    document.getElementById( 'value3' ).innerHTML=getRadomValues();
+}, 300)
