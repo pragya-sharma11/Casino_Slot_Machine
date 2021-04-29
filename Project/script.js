785cbe14e99ceb5bbdbae5c6e1f10139b066138d
github <pragya.sharma2_cs19@gla.ac.in>
2021-04-29 23:18:52 +0530

    make an array of emojis

--- Project/script.js
+++ Project/script.js
@@ -1,3 +1,12 @@
 let value1 = document.getElementsById('value1')
 let value2 = document.getElementsById('value2')
 let value3 = document.getElementsById('value3')
+
+let values = [
+    String.fromCodePoint(128514),
+    String.fromCodePoint(128516),
+    String.fromCodePoint(128526),
+    String.fromCodePoint(128534),
+    String.fromCodePoint(128525),
+    String.fromCodePoint(128520)
+]
