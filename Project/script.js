ca4e78cd8febd1cfa821729af2bc4aa84b2f9896
github <pragya.sharma2_cs19@gla.ac.in>
2021-04-29 23:51:13 +0530

    default changes

--- Project/script.js
+++ Project/script.js
@@ -1,6 +1,6 @@
-let value1 = document.getElementsById('value1')
-let value2 = document.getElementsById('value2')
-let value3 = document.getElementsById('value3')
+let value1 = document.getElementsById('value1');
+let value2 = document.getElementsById('value2');
+let value3 = document.getElementsById('value3');
 
 let arrayValues = [String.fromCodePoint(0x1F60E),String.fromCodePoint(0x1F60B),String.fromCodePoint(0x1F60D),String.fromCodePoint(0x1F608),String.fromCodePoint(0x1F602),String.fromCodePoint(0x1F604)]
 
