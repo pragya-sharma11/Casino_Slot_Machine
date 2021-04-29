04e85aa7ec688f8fb771ef46a5f0aa4f0afee94c
github <pragya.sharma2_cs19@gla.ac.in>
2021-04-29 23:26:29 +0530

    some fixes

--- Project/script.js
+++ Project/script.js
@@ -2,7 +2,7 @@ let value1 = document.getElementsById('value1')
 let value2 = document.getElementsById('value2')
 let value3 = document.getElementsById('value3')
 
-let values = [
+values = [
     String.fromCodePoint(128514),
     String.fromCodePoint(128516),
     String.fromCodePoint(128526),
